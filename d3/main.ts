2d88bf0ef357c49feadc7257dc9bd54c003cace6
Victor Suzin <victorsuzin@gmail.com>
2024-12-03 17:50:40 -0500

    d3 pt1

--- d3/main.ts
+++ d3/main.ts
@@ -1,8 +1,13 @@
-export function add(a: number, b: number): number {
-  return a + b;
+async function pt_1() :Promise<number>{
+    const txt = await Deno.readTextFile('data.txt');
+    const mul_array = txt.match(/mul\(\d{1,3},\d{1,3}\)/g);
+    let total = 0;
+
+    if(!mul_array){return 0;}
+    for(const mul of mul_array){
+        total += Number(mul.substring(4,mul.indexOf(','))) * Number(mul.substring(mul.indexOf(',') + 1, mul.length - 1));
+    }
+    return total;
 }
 
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
-if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
-}
+console.log(await pt_1());
